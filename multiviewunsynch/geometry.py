726ff6ce01de03228ec722b56a49761c818f6c20
Cenek Albl <alblc@phys.ethz.ch>
2019-03-01 10:23:01 +0100

    Initial structure, sample function and gitignore

---
+++ multiviewunsynch/geometry.py
@@ -0,0 +1,19 @@
+# Useful geometrical functions
+
+import numpy as np
+
+def X_(v):
+    """Creates a 3x3 skew symmetric matrix from the input vector  
+
+    A skew symmetric matrix has the property A^T = -A
+    3x3 skew symmetric matrices represent cross products as matrix multiplications
+    such that x_(a)*b = a x b
+    """
+    M = np.zeros((3,3))
+    M[0,1] = -v[2]
+    M[0,2] = v[1]
+    M[1,0] = v[2]
+    M[1,2] = -v[0]
+    M[2,0] = -v[1]
+    M[2,1] = v[2]
+    return M
